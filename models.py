427a403a0ddeba82d4dc3b3e29780a04fcfe087d
Nadezh_da <mamonova777@mail.ru>
2025-01-17 22:03:39 +0300

    Change main and models, and add README.md

--- models.py
+++ models.py
@@ -1,22 +1,34 @@
-import aiohttp
-import asyncio
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy.ext.asyncio import AsyncSrssion, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm.session import sessionmaker
+
+load_dotenv()
+
+PG_DSN = f"postgresql+asyncpg://{os.getenv('db_user')}:{os.getenv('db_password')}@" \
+        f"{os.getenv('db_host')}:{os.getenv('db_port')}/{os.getenv('db_name')}"
+engine = create_async_engine(PG_DSN)
+Base = declarative_base()
+Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 
-url = 'http://swapi.dev/api/people/'
+class SwapiPeople(Base):
+    __tablename__="swapi_people"
 
-
-async def get_person(session, url):
-    async with session.get(url) as file:
-        return await file.json()
-
-
-async def get_name(url):
-    async with aiohttp.ClientSession() as session:
-        async  for page in range(1, 100):
-            data = await get_person(session, url)
-            return data
-
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+    id = Column(Integer, primary_key=True)
+    birth_year = Column(String(10))
+    eye_color = Column(String(20))
+    films = Column(Text)
+    gender = Column(String(30))
+    hair_color = Column(String(30))
+    heiht = Column(String(20))
+    homeworld = Column(String(200))
+    mass = Column(String(30))
+    name = Column(String(30))
+    skin_color = Column(String(30))
+    species = Column(Text)
+    starships = Column(Text)
+    vehicles = Column(Text)
